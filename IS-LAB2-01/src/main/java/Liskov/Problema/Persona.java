cd23355b006d38452d7f3823d0772a36bac929b9
rocordonez9 <rodrigocristobal9@gmail.com>
2020-04-03 14:50:17 -0600

    Liskov-Problema

---
+++ IS-LAB2-01/src/main/java/Liskov/Problema/Persona.java
@@ -0,0 +1,30 @@
+
+package Liskov.Problema;
+
+/**
+ *
+ * @author roco
+ */
+public class Persona {
+    
+    public Persona() {
+    }
+
+    public int dpi;
+
+  
+    public String nombre;
+
+   
+    public String apellido;
+
+
+    public int tarjeta;
+
+   
+    public void pagar() {
+        // TODO implement here
+    }
+
+    
+}
